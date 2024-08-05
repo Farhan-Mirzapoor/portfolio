fcf450652eb5c38bc6dae601ab382c9e442474c1
Farhan Mirzapoor <farhan.mirzapoor.com@gmail.com>
2024-08-05 17:23:19 +0330

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({
+  params,
+}: {
+  params: { user: string };
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello world</h1>
-    </main>
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
   );
 }
